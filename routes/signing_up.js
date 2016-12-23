fb5f2cd093f7c75181df62c06927bb9e34be6157
root <nitya.nand@daffodilsw.com>
2016-12-23 14:44:56 +0530

    ok

--- routes/signing_up.js
+++ routes/signing_up.js
@@ -12,39 +12,47 @@ router.post('/', function (req, res) {
     var db = req.app.locals.db
     return Util.getRequestParams(req).then(function (params) {
         paramsProvided = params;
-        if (params.hasOwnProperty('username') && params.hasOwnProperty('password')) {
+        if (params.hasOwnProperty('username') && params.hasOwnProperty('password') && params.hasOwnProperty('email')) {
             return validateUser(params, db)
         }
     }).then(function (data) {
         validateReturnData = data
-        if (data) {
-            //req.path = "/signing_up/home"
+        if (!data.error) {
             return saveUserConnection(paramsProvided, res, db)
         }
         else {
-            res.render('already_exist');
+            res.send(data);
         }
     }).then(function(token){
-        if(validateReturnData){
+        if(!validateReturnData.error){
             res.cookie("token", token, { maxAge: 10*60*1000 });
-            req.path = '/home'
-            res.redirect('/home')
+            return finduserDetails(paramsProvided,db)
+
+        }
+    }).then(function(userData){
+        if(userData._id){
+            //req.path = '/home'
+            //res.redirect('/home')
+            res.send(userData)
         }
     })
 })
 
 
 function validateUser(params, db) {
-    if (params && params.username && params.password) {
+    if (params && params.username && params.password && params.email) {
         return findAlreadySignUpUser(params, db).then(function (data) {
-            console.log("data.result >> >>> >>> " + JSON.stringify(data))
             if (data && data._id) {
-                return false;
+                return {
+                    "success": 0,
+                    "error": true,
+                    "error_msg": "User already existed with " + data.email
+                };
 
             }
             else {
                 return insertNewUserDeatils(params, db).then(function (doc) {
-                    return true
+                    return { "error": false, user:doc, "updated_at": null }
                 })
 
             }
@@ -55,14 +63,14 @@ function validateUser(params, db) {
 
 function findAlreadySignUpUser(params, db) {
     var d = q.defer()
-    db.collection('pl.users').findOne({name: params.username}, function (err, doc) {
+    db.collection('pl.users').findOne({name: params.username,email: params.email}, function (err, doc) {
         d.resolve(doc)
     })
     return d.promise;
 }
 function insertNewUserDeatils(params, db) {
     var d = q.defer()
-    db.collection('pl.users').insertOne({name: params.username, password: params.password}, function (err, doc) {
+    db.collection('pl.users').insertOne({name: params.username, password: params.password, email: params.email,created_at:new Date(),updated_at:""}, function (err, doc) {
         d.resolve(doc)
     })
     return d.promise;
@@ -71,7 +79,6 @@ function insertNewUserDeatils(params, db) {
 
 function saveUserConnection(params, res, db) {
     var token = genrateToken()
-    console.log(">>>>>>>", token)
     var d = q.defer();
     db.collection('pl.connections').insertOne({
         token: token,
@@ -82,6 +89,21 @@ function saveUserConnection(params, res, db) {
             d.reject(err);
         } else {
             d.resolve(token);
+        }
+    });
+    return d.promise;
+}
+function finduserDetails(params, db) {
+    console.log(">>>>>>>", params)
+    var d = q.defer();
+    db.collection('pl.users').findOne({
+        name: params.username,
+        email: params.email
+    }, function (err, doc) {
+        if (err) {
+            d.reject(err);
+        } else {
+            d.resolve(doc);
         }
     });
     return d.promise;
