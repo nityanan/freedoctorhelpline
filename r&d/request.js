776b617075b9183d32022cf4a365cc4d7a712ea6
root <nitya.nand@daffodilsw.com>
2016-12-24 12:27:26 +0530

    ok

--- r&d/request.js
+++ r&d/request.js
@@ -3,9 +3,7 @@
  */
 var request = require('request');
 request('http://104.211.244.9/', function (error, response, body) {
-    console.log("response >>>>>>>>"+JSON.stringify(response))
-    console.log("response >>>>>>>>"+body)
     if (!error && response.statusCode == 200) {
-        console.log(body) // Show the HTML for the Google homepage.
+        console.log(body) // Show the HTML page.
     }
 })
